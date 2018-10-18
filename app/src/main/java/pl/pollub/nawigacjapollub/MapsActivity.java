e0f6c7760defc07160e72fa5385904ba197b1f8e
damiangalan <damiang1996@gmail.com>
2018-10-18 14:45:58 +0200

    added permissions checking

--- app/src/main/java/pl/pollub/nawigacjapollub/MapsActivity.java
+++ app/src/main/java/pl/pollub/nawigacjapollub/MapsActivity.java
@@ -1,8 +1,14 @@
 package pl.pollub.nawigacjapollub;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Camera;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,6 +25,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
 
     private GoogleMap mMap;
+    private final static int REQUEST_SMS = 100;
+    private final static int REQUEST_ACCESS_FINE_LOCATION = 200;
+    private final static int REQUEST_ACCESS_WIFI = 300;
+    private final static int REQUEST_CHANGE_WIFI = 400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -42,19 +52,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_SMS: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Nadano uprawnienia wysyłania SMS",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Nie nadano uprawnień wysyłania SMS",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            case REQUEST_ACCESS_FINE_LOCATION: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Nadano uprawnienia lokalizacji",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Nie nadano uprawnień lokalizacji",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            case REQUEST_ACCESS_WIFI: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Nadano uprawnienia stanu sieci Wi-Fi",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Nie nadano uprawnień stanu sieci Wi-Fi",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            case REQUEST_CHANGE_WIFI: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Nadano uprawnienia zmiany sieci Wi-Fi",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Nie nadano uprawnień zmiany sieci Wi-Fi",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap)
     {
         mMap = googleMap;
 
         LatLng weii = new LatLng(51.236451, 22.548368);             // Marker WEII
 
-//        LatLngBounds weiiBounds = new LatLngBounds(                       // Wierzchołki prostokąta do podmiany
-//                new LatLng(51.236451, 22.548368),                         // SW
-//                new LatLng(51.237171, 22.549522));                        // NE
+        //LatLngBounds weiiBounds = new LatLngBounds(                       // Wierzchołki prostokąta do podmiany
+        //       new LatLng(51.236451, 22.548368),                         // SW
+        //        new LatLng(51.237171, 22.549522));                        // NE
 
-//        mMap.addGroundOverlay(new GroundOverlayOptions()
-//        .image(BitmapDescriptorFactory.fromResource(R.drawable.obrazek)) // Podmiana wycinka mapy
-//        .positionFromBounds(weiiBounds));
+        //mMap.addGroundOverlay(new GroundOverlayOptions()
+        //.image(BitmapDescriptorFactory.fromResource(R.drawable.obrazek)) // Podmiana wycinka mapy
+        //.positionFromBounds(weiiBounds));
+
+        //mMap.getUiSettings().isZoomGesturesEnabled(true);                 //ustawienie zoom
 
         mMap.addMarker(new MarkerOptions().position(weii));                // Dodanie markera WEII
         mMap.moveCamera(CameraUpdateFactory.newLatLng(weii));              // Ustawienie kamery na marker WEII
