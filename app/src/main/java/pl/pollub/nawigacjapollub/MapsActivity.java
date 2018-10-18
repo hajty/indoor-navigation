fbdfee5c63d9fcbecd56952d980803fffc4f201a
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2018-10-19 00:44:59 +0200

    mapa prawie idealna

--- app/src/main/java/pl/pollub/nawigacjapollub/MapsActivity.java
+++ app/src/main/java/pl/pollub/nawigacjapollub/MapsActivity.java
@@ -1,12 +1,8 @@
 package pl.pollub.nawigacjapollub;
 
-import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -23,12 +19,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
+    public final static int REQUEST_SMS = 100;
+    public final static int REQUEST_ACCESS_FINE_LOCATION = 200;
+    public final static int REQUEST_ACCESS_WIFI = 300;
+    public final static int REQUEST_CHANGE_WIFI = 400;
 
     private GoogleMap mMap;
-    private final static int REQUEST_SMS = 100;
-    private final static int REQUEST_ACCESS_FINE_LOCATION = 200;
-    private final static int REQUEST_ACCESS_WIFI = 300;
-    private final static int REQUEST_CHANGE_WIFI = 400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,43 +48,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode){
-            case REQUEST_SMS: {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case REQUEST_SMS:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
                     Toast.makeText(getApplicationContext(), "Nadano uprawnienia wysyłania SMS",
                             Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else
+                {
                     Toast.makeText(getApplicationContext(), "Nie nadano uprawnień wysyłania SMS",
                             Toast.LENGTH_LONG).show();
                 }
             }
 
-            case REQUEST_ACCESS_FINE_LOCATION: {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            case REQUEST_ACCESS_FINE_LOCATION:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
                     Toast.makeText(getApplicationContext(), "Nadano uprawnienia lokalizacji",
                             Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else
+                {
                     Toast.makeText(getApplicationContext(), "Nie nadano uprawnień lokalizacji",
                             Toast.LENGTH_LONG).show();
                 }
             }
 
-            case REQUEST_ACCESS_WIFI: {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            case REQUEST_ACCESS_WIFI:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
                     Toast.makeText(getApplicationContext(), "Nadano uprawnienia stanu sieci Wi-Fi",
                             Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else
+                {
                     Toast.makeText(getApplicationContext(), "Nie nadano uprawnień stanu sieci Wi-Fi",
                             Toast.LENGTH_LONG).show();
                 }
             }
 
-            case REQUEST_CHANGE_WIFI: {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            case REQUEST_CHANGE_WIFI:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
                     Toast.makeText(getApplicationContext(), "Nadano uprawnienia zmiany sieci Wi-Fi",
                             Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else
+                {
                     Toast.makeText(getApplicationContext(), "Nie nadano uprawnień zmiany sieci Wi-Fi",
                             Toast.LENGTH_LONG).show();
                 }
@@ -101,23 +115,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         mMap = googleMap;
 
-        LatLng weii = new LatLng(51.236451, 22.548368);             // Marker WEII
+        LatLng weii = new LatLng(51.236451, 22.548368);                    // Marker WEII
+        LatLngBounds weiiBounds = new LatLngBounds(                               // Wierzchołki prostokąta do podmiany
+                new LatLng(51.236451, 22.548368),                           // SW
+                new LatLng(51.237171, 22.549522));                          // NE
 
-        //LatLngBounds weiiBounds = new LatLngBounds(                       // Wierzchołki prostokąta do podmiany
-        //       new LatLng(51.236451, 22.548368),                         // SW
-        //        new LatLng(51.237171, 22.549522));                        // NE
+        mMap.addGroundOverlay(new GroundOverlayOptions()
+        .image(BitmapDescriptorFactory.fromResource(R.drawable.weii_parter))        // Podmiana wycinka mapy
+        .positionFromBounds(weiiBounds));
 
-        //mMap.addGroundOverlay(new GroundOverlayOptions()
-        //.image(BitmapDescriptorFactory.fromResource(R.drawable.obrazek)) // Podmiana wycinka mapy
-        //.positionFromBounds(weiiBounds));
+        mMap.addMarker(new MarkerOptions().position(weii));                      // Dodanie markera WEII
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(weii));                  // Ustawienie kamery na marker WEII
 
-        //mMap.getUiSettings().isZoomGesturesEnabled(true);                 //ustawienie zoom
-
-        mMap.addMarker(new MarkerOptions().position(weii));                // Dodanie markera WEII
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(weii));              // Ustawienie kamery na marker WEII
-
+        //mMap.getUiSettings().isZoomGesturesEnabled(true);                       //ustawienie zoom
         // Add a marker in Sydney and move the camera
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(weii));
     }
 }
