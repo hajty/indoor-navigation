6c5b86b45934798e82501a111d251ef0b802212d
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2018-12-19 21:13:42 +0100

    nowa tabela, dodano pozostałe punkty i pokoje

--- app/src/main/java/pl/pollub/nawigacjapollub/PointsContract.java
+++ app/src/main/java/pl/pollub/nawigacjapollub/PointsContract.java
@@ -4,7 +4,7 @@ import android.provider.BaseColumns;
 
 public final class PointsContract
 {
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 9;
     public static final String DATABASE_NAME = "pollub_nav.db";
 
     private static final String TEXT_TYPE = " TEXT";
@@ -40,17 +40,33 @@ public final class PointsContract
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
 
+    public static class EdgesEntry implements BaseColumns
+    {
+        public static final String TABLE_NAME = "edges";
+
+        public static final String COLUMN_NAME_FROM_POINT_ID = "from_point_id";
+        public static final String COLUMN_NAME_TO_POINT_ID = "to_point_id";
+        public static final String COLUMN_NAME_LENGTH = "length";
+
+        public static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + " (" + BaseColumns._ID + " INTEGER PRIMARY KEY," +
+                COLUMN_NAME_FROM_POINT_ID + INTEGER_TYPE + COMMA_SEP +
+                COLUMN_NAME_TO_POINT_ID + INTEGER_TYPE + COMMA_SEP +
+                COLUMN_NAME_LENGTH + INTEGER_TYPE + ")";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
     public static class RoomsEntry implements BaseColumns
     {
         public static final String TABLE_NAME = "rooms";
 
-        public static final String COLUMN_ROOMS_NAMES = "rooms_names";
         public static final String COLUMN_NAME_ID_POINTS = "id_points";
+        public static final String COLUMN_ROOM_NAME = "room_name";
 
         public static final String CREATE_TABLE = "CREATE TABLE " +
                 TABLE_NAME + " (" + BaseColumns._ID + " INTEGER PRIMARY KEY," +
-                COLUMN_ROOMS_NAMES + INTEGER_TYPE + COMMA_SEP +
-                COLUMN_NAME_ID_POINTS  + TEXT_TYPE + ")";
+                COLUMN_NAME_ID_POINTS+ INTEGER_TYPE + COMMA_SEP +
+                COLUMN_ROOM_NAME  + TEXT_TYPE + ")";
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
 }
